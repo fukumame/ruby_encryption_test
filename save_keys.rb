f51a46c029606b3be491c01734e5f93df2086df3
fukumame <tanuma0115@gmail.com>
2018-04-07 23:54:10 +0900

    tweak

--- save_keys.rb
+++ save_keys.rb
@@ -15,12 +15,12 @@ encrypted_private_key = rsa.export(OpenSSL::Cipher.new("aes256"), passphrase)
 puts encrypted_private_key
 
 # 出力した秘密鍵(暗号化済)をファイルに書き込み
-File.open("./private_key.pem", "w") do |f|
+File.open("keys/private_key.pem", "w") do |f|
   f.write(encrypted_private_key)
 end
 
 # 公開鍵をpublic_key.pemに保存
 public_key = rsa.public_key
-File.open("public_key.pem", "w") do |f|
+File.open("keys/public_key.pem", "w") do |f|
   f.write(public_key.export)
 end
--- use_keys.rb
+++ use_keys.rb
@@ -1,14 +1,14 @@
 require 'openssl'
 
 password = "!secret passphrase!"
-private_key =  File.read('private_key.pem')
-public_key = File.read('public_key.pem')
+private_key =  File.read('keys/private_key.pem')
+public_key = File.read('keys/public_key.pem')
 
 # 秘密鍵によるrsaオブジェクトの生成
 private_rsa = OpenSSL::PKey::RSA.new(private_key, password )
 
 # 公開鍵によるrsaオブジェクトの取得
-public_rsa = OpenSSL::PKey::RSA.new(public_key, password )
+public_rsa = OpenSSL::PKey::RSA.new(public_key)
 
 # 暗号化対象の文字列
 secret_text = "This is a secret text."
@@ -27,14 +27,16 @@ puts "encrypted by public => #{encrypted_text_pub}"
 # 秘密鍵による復号化
 puts  "decrypted by private => #{private_rsa.private_decrypt(encrypted_text_pub)}"
 
-
 begin
   # この処理は公開鍵であるにもかかわらず、秘密鍵での暗号化処理を行っているため失敗する
   public_rsa.private_encrypt(secret_text)
+rescue => e
+  puts "Error Occurred => #{e}"
+end
 
+begin
   # この処理も公開鍵であるにもかかわらず、秘密鍵での復号化処理を行っているため失敗する
   public_rsa.private_decrypt(encrypted_text_pub)
 rescue => e
   puts "Error Occurred => #{e}"
 end
-
